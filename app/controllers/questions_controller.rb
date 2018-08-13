7c047069767b7f8f876ce643b12dba993e6e9064
Luiza Pinto <luizalimapinto@gmail.com>
2018-08-13 13:37:16 +0100

    works

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @coach_answer = ""
+    @question = params[:question]
+    if @question == 'I am going to work'
+      @coach_answer = 'Great!'
+    elsif @question.include?('?')
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
